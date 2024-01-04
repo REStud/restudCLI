b2fcd0dd80a4f9e1f544d0eff2be52358d302722
Gergely Attila Kiss <corra971407@gmail.com>
2024-01-04 10:52:11 +0100

    setting up new repo (#6)

--- .config/fish/functions/restud.fish
+++ .config/fish/functions/restud.fish
@@ -26,5 +26,11 @@ function restud
             else
                 xargs .zenodo | curl -Lo repo.zip 
             end
+        case new
+           mkdir $argv[2] 
+           cd $argv[2]
+           git init
+           gh repo create restud-replication-packages/$argv[2] --private --team Replicators
+           git remote add origin git@github.com:restud-replication-packages/$argv[2].git
     end
 end
