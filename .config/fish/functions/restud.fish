db4e1ed05940e76cb201f23d26dbab9861ce8f46
Miklós Koren <miklos.koren@gmail.com>
2024-01-08 10:09:18 +0100

    restud init downloads the latest version from github (fix #11)

--- .config/fish/functions/restud.fish
+++ .config/fish/functions/restud.fish
@@ -1,5 +1,17 @@
 function restud
     switch $argv[1]
+        case init
+            set temp_folder (mktemp -d)
+            cd $temp_folder
+            git clone git@github.com:REStud/workflow.git
+            cp -r workflow/.config/restud ~/.config/
+            cp workflow/.config/fish/functions/restud.fish ~/.config/fish/functions/
+            set -Ux RESTUD ~/.config/restud
+            echo "Please add the following line to your .config/fish/config.fish file:"
+            echo "set -Ux RESTUD ~/.config/restud"
+            echo "Then restart your terminal."
+            cd -
+            rm -rf $temp_folder
         case pull
             if not test -d $argv[2]
                 git clone git@github.com:restud-replication-packages/$argv[2].git
