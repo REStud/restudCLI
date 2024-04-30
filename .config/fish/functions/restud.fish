9fd77a01c33a604f13ac6cc82e1b390dbf65215c
Gergely Attila Kiss <corra971407@gmail.com>
2024-04-30 11:02:57 +0200

    add working updates

--- .config/fish/functions/restud.fish
+++ .config/fish/functions/restud.fish
@@ -83,7 +83,8 @@ function restud
                 git commit -m "update to zenodo version $argv[2]"
                 git push 
                 restud _get_latest_version
-                git checkout -b verion$v
+                set -gx v (math $v + 1)
+                git checkout -b version$v
             end
             restud _save_id
         case report $argv[2]
@@ -92,46 +93,38 @@ function restud
             git push origin $argv[2]
         # private functions not exposed to end user
         case _get_key
-            set -gx ZENODO_API_KEY (head -n1 ~/.config/.zenodo_api_key)
+            set -gx ZENODO_API_KEY (head -n 1 ~/.config/.zenodo_api_key)
         case _download_zenodo
-            if not test -f .zenodo
-                curl -Lo repo.zip "$argv[2]?access_token=$ZENODO_API_KEY"
-                echo "$argv[2]" > .zenodo
-            else
-                curl -Lo repo.zip (head -n1 .zenodo)"?access_token=$ZENODO_API_KEY"
-            end
+            curl -Lo repo.zip "$argv[2]?access_token=$ZENODO_API_KEY"
+            echo "$argv[2]" > .zenodo
             unzip repo.zip
             rm repo.zip
         case _download_zenodo_preview
             restud _get_cookie
-             if not test -f .zenodo
-                curl -b "session=$cookie_value" -Lo repo.zip "$argv[2]"
-                echo "$argv[2]" > .zenodo
-            else
-                curl -b "session=$cookie_value" -Lo repo.zip (head -n1 .zenodo)
-            end
+            curl -b "session=$cookie_value" -Lo repo.zip "$argv[2]"
+            echo "$argv[2]" > .zenodo
             unzip repo.zip
             rm repo.zip
         case _get_cookie
             if not test -f ~/.config/restud-cookie.json
                 restud _create_cookie
             end
-            set expr_dt (jq .expiration-date ~/.config/restud-cookie.json)
-            set today () # TODO get today's date
-            if test (date -d $expr_dt +%s) -ge (date -d $today +%s)
+            set -x expr_dt (jq .exp_date ~/.config/restud-cookie.json | string replace \" "" -a)
+            set -x today (date +%Y-%m-%d) 
+            if not test (date -d $expr_dt +%s) -gt (date -d $today +%s)
                 restud _create_cookie
             end
-            set -x cookie_value (jq .value ¬/.config/restud-cookie.json)
+            set -gx cookie_value (jq .value ~/.config/restud-cookie.json | string replace \" "" -a)
         case _create_cookie
-            echo \n\n\n"Your restud cookie either does not exist or expired, to download you need to create a new one!"\n\n\n
+            echo \n\n\n"Your restud cookie either does not exist or expired, to download preview records you need to create a new one!"\n\n\n
             read -P "Do you want to create a new one into ~/.config/restud-cookie.json? (y/n)" -n 1 -x confirm
             if test $confirm != "y"
                     return 1
             end
             echo "To create a new cookie you need two values. The zenodo session cookie value and expiration date. You can access them by opening zenodo.org, logging in and then access the developer tools panel. You can open it by F12/ctrl+shift+i or through settings/more tools/developer tools. Then look for Application sheet in the developer tools, in that sheet look for Storage/Cookies. It should list all active cookies for the page, there you need the cookie named session."
-            read -P "Please copy cookie value:" -x value
-            read -P "Please copy expiration date (YYYY-MM-DD):" expr_dt
-            echo '{"name":"session", "value":$value, "expiration-date":$expr_dt}' | jq . > ~/.config/restud-cookie.json
+            read -P "Please copy cookie value: " -x value
+            read -P "Please copy expiration date: " -x expr_dt
+            jq -n --arg value $value --arg date $expr_dt '{"name":"session", "value":$value, "exp_date":$date}' > ~/.config/restud-cookie.json
         case _commit
             find . -type f -size +20M | cut -c 3- > .gitignore
             git add .
@@ -156,7 +149,6 @@ function restud
                     return 1
                 end
                 restud _community_accept
-                # if failed notifiy the user.
             else
                 echo \n\n\n"Already part of REStud community!"\n\n\n
             end
@@ -164,16 +156,13 @@ function restud
             restud _get_id
             restud _get_key
             set url "https://zenodo.org/api/records/$ZENODO_ID/requests"
-            curl "$url?access_token=$ZENODO_API_KEY" | jq --arg zendod_id $ZENODO_ID '.hits.hits[].links.actions.accept' > .accept_request # TODO: filter for restud community
+            curl "$url?access_token=$ZENODO_API_KEY" | jq --arg zendod_id $ZENODO_ID '.hits.hits[].links.actions.accept' > .accept_request
         case _community_accept 
             restud _get_accept_request
             set url (head .accept_request | string replace \" "" -a)
             curl -X POST "$url?access_token=$ZENODO_API_KEY"
             rm .accept_request
         case _get_latest_version
-            set -x v 1
-            while git switch version$v
-                set -x v (math $v + 1)
-            end
+            set -gx v (git branch -r | grep 'version' | grep -o -E '[0-9]+' | tail -1 )
     end
 end
