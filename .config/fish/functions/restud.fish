15ba90e98f71942862c1f797d36b6c42d6817a4c
Gergely Attila Kiss <corra971407@gmail.com>
2024-04-30 11:57:36 +0200

    resolve #37, and updates to date

--- .config/fish/functions/restud.fish
+++ .config/fish/functions/restud.fish
@@ -106,25 +106,25 @@ function restud
             unzip repo.zip
             rm repo.zip
         case _get_cookie
-            if not test -f ~/.config/restud-cookie.json
+            if not test -f ~/.config/restud/restud-cookie.json
                 restud _create_cookie
             end
-            set -x expr_dt (jq .exp_date ~/.config/restud-cookie.json | string replace \" "" -a)
-            set -x today (date +%Y-%m-%d) 
+            set -x expr_dt (jq .exp_date ~/.config/restud/restud-cookie.json | string replace \" "" -a)
+            set -x today (date -I) 
             if not test (date -d $expr_dt +%s) -gt (date -d $today +%s)
                 restud _create_cookie
             end
-            set -gx cookie_value (jq .value ~/.config/restud-cookie.json | string replace \" "" -a)
+            set -gx cookie_value (jq .value ~/.config/restud/restud-cookie.json | string replace \" "" -a)
         case _create_cookie
             echo \n\n\n"Your restud cookie either does not exist or expired, to download preview records you need to create a new one!"\n\n\n
-            read -P "Do you want to create a new one into ~/.config/restud-cookie.json? (y/n)" -n 1 -x confirm
+            read -P "Do you want to create a new one into ~/.config/restud/restud-cookie.json? (y/n)" -n 1 -x confirm
             if test $confirm != "y"
                     return 1
             end
-            echo "To create a new cookie you need two values. The zenodo session cookie value and expiration date. You can access them by opening zenodo.org, logging in and then access the developer tools panel. You can open it by F12/ctrl+shift+i or through settings/more tools/developer tools. Then look for Application sheet in the developer tools, in that sheet look for Storage/Cookies. It should list all active cookies for the page, there you need the cookie named session."
+            echo "\n\n\nTo create a new cookie you need two values. The zenodo session cookie value and expiration date. \n You can access them by opening zenodo.org, logging in and then access the developer tools panel. You can open it by F12/ctrl+shift+i or through settings/more tools/developer tools. Then look for Application sheet in the developer tools, in that sheet look for Storage/Cookies. It should list all active cookies for the page, there you need the cookie named session.\n\n\n"
             read -P "Please copy cookie value: " -x value
             read -P "Please copy expiration date: " -x expr_dt
-            jq -n --arg value $value --arg date $expr_dt '{"name":"session", "value":$value, "exp_date":$date}' > ~/.config/restud-cookie.json
+            jq -n --arg value $value --arg date $expr_dt '{"name":"session", "value":$value, "exp_date":$date}' > ~/.config/restud/restud-cookie.json
         case _commit
             find . -type f -size +20M | cut -c 3- > .gitignore
             git add .
