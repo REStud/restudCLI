9ccf11c37840665f90102e486b8754d9e7c00ee7
Gergely Attila Kiss <corra971407@gmail.com>
2025-03-10 16:39:10 +0100

    #22: singular replacement for cases when report has single element lists

--- .config/restud/render.py
+++ .config/restud/render.py
@@ -13,6 +13,29 @@ def parse(value):
     else:
         return value
 
+def singulars_and_plurals(template, content):
+    requests_count = 1
+    recommendations_count = 1
+    print(content)
+
+    # Check if the items are lists and count them
+    if isinstance(content.get('requests', []), list):
+        requests_count = len(content['requests'])
+    
+    if isinstance(content.get('recommendations', []), list):
+        recommendations_count = len(content['recommendations'])
+        
+    # Replace the template text with appropriate singular/plural forms
+    if requests_count == 1:
+        template = template.replace("please make the following changes:", 
+                                    "please make the following change:")
+    
+    if recommendations_count == 1:
+        template = template.replace("please consider the following recommendations", 
+                                   "please consider the following recommendation")
+    
+    return template
+
 if __name__ == "__main__":
     template = open(sys.argv[1], 'rt', encoding='utf-8').read()
     # template.yaml defines some constants that can be referred to in later .yaml 
@@ -24,6 +47,5 @@ if __name__ == "__main__":
 
     parsed_content = {k: parse(content[k]) for k in content}
 
-    print(template.format(**parsed_content))
-
-    
+    template = singulars_and_plurals(template, content)
+    print(template.format(**parsed_content))    
